95a8cfa7143324e194325ecc386cd57666427676
zsuperxtreme <61944978+zsuperxtreme@users.noreply.github.com>
2024-05-04 17:11:25 +0800

    Update config.php

--- api/config.php
+++ api/config.php
@@ -1,9 +1,47 @@
 <?php
 
 $conn = mysql_connect('43.239.233.248',
-'zack', 
-'110681' 
+  'zack', 
+  '110681' 
 ) or die(mysql_error());
 mysql_select_db('misc');
 
+$question   = $_REQUEST["question"];
+
+if($action == "new")
+{
+    $sql = "
+        INSERT INTO lundbeck(
+            question
+        )
+        VALUES(
+            '$question'
+        )
+    ";
+
+    mysql_query($sql, $conn);
+}
+
+if($action == "list")
+{
+    $sql = "SELECT * FROM player ORDER BY id DESC";	        
+    $result = mysql_query($sql, $conn);
+    while($row = mysql_fetch_assoc($result))
+    {
+        $data[] = array(
+            "id" => $row["id"],
+            "user" => $row["user"],
+            "datetime" => $row["datetime"]
+        );
+    }
+
+    echo json_encode($data, JSON_PRETTY_PRINT);
+}
+
+
+if($action == "delete")
+{
+    $sql = "DELETE FROM player WHERE id=$id";	        
+    mysql_query($sql, $conn);
+}
 ?>
